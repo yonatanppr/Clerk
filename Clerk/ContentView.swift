85f67b03e25bd847271e75b624096c020663ca3f
Yonatan Pepper <yonatan.pepper@gmail.com>
2025-06-01 17:39:40 +0200

    - added background image and welcome text

--- Clerk/ContentView.swift
+++ Clerk/ContentView.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("CLERK!")
+                .font(.system(size: 40, weight: .bold))
+                .foregroundStyle(.white)
         }
+        .background(Image("AppBackground"))
         .padding()
     }
 }
