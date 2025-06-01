98e5b001604c27620e710b0cfe31a5342f6051c7
Yonatan Pepper <yonatan.pepper@gmail.com>
2025-06-01 17:21:05 +0200

    Initial commit

---
+++ Clerk/ClerkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClerkApp.swift
+//  Clerk
+//
+//  Created by Yonatan Pepper on 01.06.25.
+//
+
+import SwiftUI
+
+@main
+struct ClerkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClerkTests/ClerkTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClerkTests.swift
+//  ClerkTests
+//
+//  Created by Yonatan Pepper on 01.06.25.
+//
+
+import Testing
+@testable import Clerk
+
+struct ClerkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
